80a3df066ff60ca04e91a71c19c13314c0929df1
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-12-13 01:50:38 +0530

    10th

--- Server/routes/Answers.js
+++ Server/routes/Answers.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {postAnswer, deleteAnswer} from '../controllers/Answers.js';
+import auth from '../middlewares/auth.js';
 
 const router =express.Router();
 
-router.patch('/post/:id',postAnswer);
-router.patch('/delete/:id',deleteAnswer)
+
+router.patch('/post/:id', auth , postAnswer);
+router.patch('/delete/:id', auth, deleteAnswer)
 
 export default router;
--- Server/routes/Questions.js
+++ Server/routes/Questions.js
@@ -1,14 +1,15 @@
 import express from 'express';
 
-import {getAllquestions, AskQuestion, deleteQuestion} from  '../controllers/Questions.js';
-// import {getAllquestions} from  '../controllers/Questions.js'
+import {getAllquestions, AskQuestion, deleteQuestion, voteQuestion} from  '../controllers/Questions.js';
+import auth from '../middlewares/auth.js';
 
 
 const router=express.Router();
 
-router.post('/Ask',AskQuestion);
+router.post('/Ask',auth,AskQuestion);
 router.get('/get', getAllquestions);
-router.delete('/delete/:id' , deleteQuestion)
+router.delete('/delete/:id' ,auth, deleteQuestion);
+router.patch('/vote/:id', auth, voteQuestion)
 
 
 export default router;
